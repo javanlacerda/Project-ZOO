7f0828e00d82d540c7d32505c0ebae734b60e838
javanlacerda <javan.lacerda@ccc.ufcg.edu.br>
2018-05-09 20:04:21 -0300

    implementada classe zoo keeper

--- CCTZoo/src/ZooKeeper.java
+++ CCTZoo/src/ZooKeeper.java
@@ -6,6 +6,8 @@ import java.util.Set;
 public class ZooKeeper {
 
 	final private int maxTypes = 3;
+	final private int numberOfAnimals = 10;
+
 	private boolean qualificado;
 	private Map<Long, Animal> animaisSobGuarda;
 	private Set<AnimalType> types;
@@ -35,14 +37,75 @@ public class ZooKeeper {
 
 		if (types.size() < maxTypes || types.contains(animal.getType())) {
 
-			animaisSobGuarda.put(animal.getExhibitNumber(), animal);
-			types.add(animal.getType());
+			if (animaisSobGuarda.size() < 10) {
 
+				animaisSobGuarda.put(animal.getExhibitNumber(), animal);
+				types.add(animal.getType());
+
+			} else {
+
+				throw new Exception("Number of animals exceded!");
+			}
 		} else {
 
-			throw new Exception();
+			throw new Exception("Numeber of types exceded!");
 		}
 
+	}
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ZooKeeper other = (ZooKeeper) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public Map<Long, Animal> getAnimaisSobGuarda() {
+		return animaisSobGuarda;
+	}
+
+	public void setAnimaisSobGuarda(Map<Long, Animal> animaisSobGuarda) {
+		this.animaisSobGuarda = animaisSobGuarda;
+	}
+
+	public Set<AnimalType> getTypes() {
+		return types;
+	}
+
+	public void setTypes(Set<AnimalType> types) {
+		this.types = types;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
