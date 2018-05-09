4af37472490f79c94178d34398a30fc5220f137d
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-05-08 22:26:02 -0300

    Adicionado Classe Abstrata Animal e Enum de Gender

---
+++ CCTZoo/src/Animal.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.util.Set;
+
+public abstract class Animal {
+
+	private String nome;
+	private Gender GENDER;
+	private double height;
+	private double weigth;
+	private double length;
+	private LocalDate dateOfBirth;
+	private LocalDate dateOfArrival;
+	private Set vaccineSet;
+	
+	
+	
+
+}
+
+
